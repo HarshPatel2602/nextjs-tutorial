846ddff163ee072479cb85834824bc3f8874cf20
HarshPatel2602 <harshpatidar2602@gmail.com>
2024-05-15 12:31:35 +0530

    second program

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,35 @@
+"use client";
 import styles from "./page.module.css";
+import { useState } from "react";
+
+// export default function Home() {
+//   return (
+//     <main className={styles.main}>
+//       <h1>Event, function and state</h1>
+//       <button onClick={()=>alert("Hello next js")}>Click Me</button>
+//     </main>
+//   );
+// }
 
 export default function Home() {
+  const [name, setName] = useState("Harsh");
+  // let data = "Harsh"
+  const apple = () => {
+    // data = "Patel"
+    setName("Patel");
+  };
+
+  const InnerComp = () => {
+    return (
+      <h1>Inner Component</h1>
+    )
+  }
   return (
     <main className={styles.main}>
-      <User name="Harsh" />
-      <User name="Patel" />
-      <h1>Home Page</h1>
+      <h1>Event, function and state {name}</h1>
+      <button onClick={() => apple()}>Click Me</button>
+      {/* <InnerComp /> */}
+      {InnerComp()}
     </main>
   );
 }
-
-const User = (props) => {
-  return (
-    <div>
-      <h2>User name is {props.name}</h2>
-    </div>
-  );
-};
