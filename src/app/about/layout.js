f38799f671f7cf4ad80391db4d7ad6f4aa6923e0
HarshPatel2602 <harshpatidar2602@gmail.com>
2024-05-29 16:08:02 +0530

    six commit

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -1,23 +1,38 @@
+"use client";
 import Link from "next/link";
-import "./about.css"
+import "./about.css";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Layout({ children }) {
+  const pathName = usePathname();
+
+  const router = useRouter();
+  const navigate = (name) => {
+    router.push(name);
+  };
+
   return (
     <div>
-      <ul className="about-menu">
-        <li>
-          <h3>About Menu</h3>
-        </li>
-        <li>
-          <Link href="/about">About Main</Link>
-        </li>
-        <li>
-          <Link href="/about/aboutcollege">About Collage</Link>
-        </li>
-        <li>
-          <Link href="/about/aboutstudent">About Student</Link>
-        </li>
-      </ul>
+      {pathName !== "/about/aboutstudent" ? (
+        <ul className="about-menu">
+          <li>
+            <h3>About Menu</h3>
+          </li>
+          <li>
+            <Link href="/about">About Main</Link>
+          </li>
+          <li>
+            <Link href="/about/aboutcollege">About Collage</Link>
+          </li>
+          <li>
+            <Link href="/about/aboutstudent">About Student</Link>
+          </li>
+        </ul>
+      ) : (
+        <button onClick={() => navigate("/about")}>
+          Go To Main About Page
+        </button>
+      )}
       {children}
     </div>
   );
