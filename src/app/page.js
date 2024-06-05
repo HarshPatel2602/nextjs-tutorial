a8e0b802c2606f7a84f0ea14a86f0595ffe6ab6f
HarshPatel2602 <harshpatidar2602@gmail.com>
2024-06-05 18:34:18 +0530

    twelve commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>Fetch Data With API in Client Component</h1>
+      <h1>Fetch Data With API in Server Component</h1>
       <Link href="/productlist">Go to Product List</Link>
     </main>
   );
--- src/app/productlist/page.js
+++ src/app/productlist/page.js
@@ -1,22 +1,14 @@
-"use client";
-import { useEffect, useState } from "react";
+// Call API in Server Component
 
-export default function Page() {
-  const [products, setProducts] = useState([]);
+const productList=async ()=> {
+  let data = await fetch("https://dummyapi.online/api/pokemon");
+  data = await data.json();
+  return data;
+}
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        let data = await fetch("https://dummyapi.online/api/pokemon");
-        data = await data.json();
-        setProducts(data);
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      }
-    };
-
-    fetchProducts();
-  }, []);
+export default async function Page() {
+  const products = await productList();
+//   console.log(products);
 
   return (
     <div>
@@ -32,3 +24,42 @@ export default function Page() {
     </div>
   );
 }
+
+
+
+// Call API in Client Component
+
+// "use client";
+// import { useEffect, useState } from "react";
+
+// export default function Page() {
+//   const [products, setProducts] = useState([]);
+
+//   useEffect(() => {
+//     const fetchProducts = async () => {
+//       try {
+//         let data = await fetch("https://dummyapi.online/api/pokemon");
+//         data = await data.json();
+//         setProducts(data);
+//       } catch (error) {
+//         console.error("Error fetching products:", error);
+//       }
+//     };
+
+//     fetchProducts();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Product List</h1>
+//       {products.map((item) => (
+//         <div key={item.id}>
+//           <h1>Pokemon : {item.pokemon}</h1>
+//           <h2>Type : {item.type}</h2>
+//           <h3>Abilities : {item.abilities}</h3>
+//           <h4>Hitpoints : {item.hitpoints}</h4>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
