d275c8a66ee565123f25858e5030a8772297cbb6
HarshPatel2602 <harshpatidar2602@gmail.com>
2024-06-06 14:36:07 +0530

    fifteen

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
+import custom from "./custom.module.css"
+import other from "./other.module.css"
+import outside from "@/style/outside.module.css"
+
 export default function Home() {
   return (
     <main>
-      <h1>Style and CSS in Next JS</h1>
-      <h2>Heading 2 in Main Page</h2>
+      <h1 className={custom.main}>CSS Modules with Next JS</h1>
+      <h2 className={other.main}>Heading 2 in Main Page</h2>
+      <h3 className={outside.main}>Heading 3 in Main Page</h3>
     </main>
   );
 }
