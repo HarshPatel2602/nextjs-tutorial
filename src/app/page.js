b668c420856302f1a2b856f82f69b9fa8619e2cd
HarshPatel2602 <harshpatidar2602@gmail.com>
2024-06-10 12:34:02 +0530

    eighteen commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,18 @@
-import Image from "next/image";
-import Profile from "../../public/vercel.svg";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight:'100',
+  subsets: ["latin"],
+  display:'swap'
+})
 
 export default function Home() {
   return (
     <main>
-      <h1>Image Optimization in next.js</h1>
-
-      {/* <Image src={Profile} />
-      <img src={Profile.src} /> */}
-
-      <Image
-        src="https://images.pexels.com/photos/1270184/pexels-photo-1270184.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        width={1800}
-        height={1800}
-      />
+      <h1>Font Optimization in next.js</h1>
+      {/* <h1 style={{fontFamily:'Roboto', fontWeight:100}}>Font with Link Tag</h1> */}
+      
+      <h1 className={roboto.className}>Font with Next JS font feature</h1>
     </main>
   );
 }
