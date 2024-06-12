ed3ac7d0a1a58bdf7b19a756d736dda3d5060e16
HarshPatel2602 <harshpatidar2602@gmail.com>
2024-06-12 16:11:41 +0530

    twenty-three commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,7 @@
-// import Image from "next/image";
-// import Photo from "../../public/dummy.jpg"
-
 export default function Home() {
   return (
     <main>
-      <h1>Static Assets in Next</h1>
-      {/* <Image src={Photo} alt="" width={100} hight={100} /> */}
-      <img src="/dummy.jpg" alt="Description of image" width={100} height={100} />
+      <h1>Export Static HTML Page with Build</h1>
     </main>
   );
 }
---
+++ src/app/seller/page.js
@@ -0,0 +1,13 @@
+import User from "@/components/user";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Seller</h1>
+      <User />
+    </div>
+  );
+};
+
+export default Page;
