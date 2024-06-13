a935a425255f704145791d30e9968d0768fc914c
HarshPatel2602 <harshpatidar2602@gmail.com>
2024-06-13 18:59:30 +0530

    twenty-six commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    redirects: async()=>{
-        return [
-            {
-                source: '/user',
-                destination: '/',
-                permanent: false
-            },
-            {
-                source: '/user/:userid',
-                destination: '/',
-                permanent: false
-            },
-            {
-                source: '/admin',
-                destination: '/',
-                permanent: false
-            },
-        ]
-    }
+    
 };
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,18 @@
+import { API_BASE_URL } from "@/config/constants";
+
 export default function Home() {
+  console.log(process.env.CUSTOM_MODE);
+
   return (
     <main>
-      <h1>Redirection in Next JS</h1>
+      {process.env.NODE_ENV === "development" ? (
+        <h1>Development Mode</h1>
+      ) : (
+        <h1>Production Mode</h1>
+      )}
+      <h1>Environment Variables in Next JS</h1>
+
+      <h1>{API_BASE_URL}</h1>
     </main>
   );
 }
