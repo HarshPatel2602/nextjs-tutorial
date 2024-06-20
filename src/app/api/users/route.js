ecd49b044e62a28e0f433e25435b3f018283fa2a
HarshPatel2602 <harshpatidar2602@gmail.com>
2024-06-20 12:23:10 +0530

    thirty-two commit

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -8,9 +8,15 @@ export const GET = () => {
 
 export const POST = async (request) => {
   const payload = await request.json();
-  console.log(payload.name)
-  if(!payload.name || !payload.age || !payload.email || !payload.city){
-    return NextResponse.json({result:"Required field not found", success:false}, {status:400})
+  console.log(payload.name);
+  if (!payload.name || !payload.age || !payload.email || !payload.city) {
+    return NextResponse.json(
+      { result: "Required field not found", success: false },
+      { status: 400 }
+    );
   }
-  return NextResponse.json({result:"New user created", success:true}, {status:201})
-}
+  return NextResponse.json(
+    { result: "New user created", success: true },
+    { status: 201 }
+  );
+};
