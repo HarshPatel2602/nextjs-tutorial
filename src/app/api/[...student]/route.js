63cb78db1c97f53985647e5a1b61ebb678d2f530
HarshPatel2602 <harshpatidar2602@gmail.com>
2024-06-24 14:33:56 +0530

    thirty-six commit

---
+++ src/app/api/[...student]/route.js
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export const GET = async (request, content) => {
+  const studentDetails = content.params.student;
+  console.log(studentDetails);
+  return NextResponse.json(
+    { result: studentDetails, success: true },
+    { status: 200 }
+  );
+};
