487776b022c523418eda332453ed8fc83ed6d026
HarshPatel2602 <harshpatidar2602@gmail.com>
2024-07-02 15:11:36 +0530

    thirty-nine commit

---
+++ src/app/addproduct/page.js
@@ -0,0 +1,84 @@
+"use client";
+import { useState } from "react";
+import "../style.css";
+
+const Page = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [color, setColor] = useState("");
+  const [company, setCompany] = useState("");
+  const [category, setCategory] = useState("");
+
+  const addProduct = async () => {
+    if (!name || !price || !color || !company || !category) {
+      alert("Please fill out all fields.");
+      return;
+    }
+
+    let result = await fetch("http://localhost:3000/api/products", {
+      method: "POST",
+      body: JSON.stringify({
+        name,
+        price,
+        color,
+        company,
+        category,
+      }),
+    });
+    result = await result.json();
+    if (result.success) {
+      alert("Product Added Successfully");
+      setName("");
+      setPrice("");
+      setColor("");
+      setCompany("");
+      setCategory("");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Add Products</h1>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Product Name"
+        className="input"
+      />
+      <input
+        type="text"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+        placeholder="Product Price"
+        className="input"
+      />
+      <input
+        type="text"
+        value={color}
+        onChange={(e) => setColor(e.target.value)}
+        placeholder="Product Color"
+        className="input"
+      />
+      <input
+        type="text"
+        value={company}
+        onChange={(e) => setCompany(e.target.value)}
+        placeholder="Product Company"
+        className="input"
+      />
+      <input
+        type="text"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+        placeholder="Product Category"
+        className="input"
+      />
+      <button className="btn" onClick={addProduct}>
+        Add Product
+      </button>
+    </div>
+  );
+};
+
+export default Page;
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
 
   return (
     <main>
-        <h1>API Routes</h1>
+        <Link href="/addproduct">Add Product</Link>
     </main>
   );
 }
